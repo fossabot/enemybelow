8166ea222d39a5c5319290cf824b0cab99f9e6a8
Chris Etzel <aragon@aragonseye.com>
2022-08-28 20:31:03 -0500

    finished most work on movement. Renamed the function diagonal to 'move_object'

--- movement.py
+++ movement.py
@@ -1,4 +1,4 @@
-def diagonal(position, speed, direction):
+def move_object(position, speed, direction):
 
 
 	# start and end points of the board
@@ -25,20 +25,59 @@ def diagonal(position, speed, direction):
 			position -= 9
 			speed -= 1
 
+	#NORTHWEST
 	elif direction == "NW":
 		while speed > 0:
 			position -= 11
 			speed -= 1
-	
+
+	#SOUTH
+	elif direction == "S":
+		if (position > 90):
+			direction = input("You can't navigate any further SOUTH. ")
+
+		while speed > 0:
+			position += 10
+			speed -= 1
+
+	#SOUTHEAST
 	elif direction == "SE":
 		while speed > 0:
 			position += 11
 			speed -= 1
 
+	#SOUTHWEST
 	elif direction == "SW":
 		while speed > 0:
 			position += 9
 			speed -= 1
 
+	#EAST
+	elif direction == "E":
+		if position % 10 == 0:
+			direction = input("You can't navigate any further EAST. ")
+
+		while speed > 0:
+			position += 1
+			speed -= 1
+
+	#WEST
 	
+	elif direction == "W": 
+		# west walls are 1,11,21,31,41,51...and we don't want them to hit them
+		west_walls = [1,11,21,31,41,51,61,71,81,91]
+		
+		for w in west_walls:
+			if position == w:
+				direction = input("You can't navigate any further WEST. ")
+
+	while speed > 0:
+		position -= 1
+		speed -= 1
+
+		
 	return position
+
+myMove = move_object(41,2,"W")
+
+print(myMove)
