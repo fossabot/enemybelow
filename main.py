1f4767bf3a1795c698158834b6ef35c257648436
Aragon Etzel <101592323+aragonseye@users.noreply.github.com>
2022-08-28 21:06:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,29 +23,31 @@ if sub_position == destroyer_position:
 	sub_position = random.randrange(sub_position, 99, 1)
 
 # start the game !
+
+print(
+	'''
+	Captain! A submarine has been spotted in the area. 
+	To hunt, follow the game prompts to enter the new
+	speed and direction you want to go. You can go any
+	direction (N,S,E,W) and (NE,SE,NW,SW) and your
+	speed is variable between 1 - 9 knots. A knot is 
+	a grid-square reference. For instance, if you're 
+	on grid square reference 54, setting your direction
+	WEST at 4 knots will put you at grid square reference
+	50. Each time you move, the enemy sub will move 1 space
+	in any direction. Your job is to get within 2 grid
+	squares from the sub and fire. Good luck!
+
+	Now, let's get some bearings...
+
+	...<<PING>>...
+	The enemy submarine is currently at grid square reference
+	''', sub_position)
+
+print("Captain, our current position is ", destroyer_position)
+print("Now we need to input our new course to chase the enemy!")
+
 while True: 
-		print(
-		'''
-		Captain! A submarine has been spotted in the area. 
-		To hunt, follow the game prompts to enter the new
-		speed and direction you want to go. You can go any
-		direction (N,S,E,W) and (NE,SE,NW,SW) and your
-		speed is variable between 1 - 9 knots. A knot is 
-		a grid-square reference. For instance, if you're 
-		on grid square reference 54, setting your direction
-		WEST at 4 knots will put you at grid square reference
-		50. Each time you move, the enemy sub will move 1 space
-		in any direction. Your job is to get within 2 grid
-		squares from the sub and fire. Good luck!
-
-		Now, let's get some bearings...
-
-		...<<PING>>...
-		The enemy submarine is currently at grid square reference
-		''', sub_position)
-
-		print("Captain, our current position is ", destroyer_position)
-		print("Now we need to input our new course to chase the enemy!")
 		
 		new_direction = input("Captain, what direction should we head? ") 
 		new_speed = int(input("What speed, captain ? 1 - 9 knots: "))
