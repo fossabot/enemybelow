5bb59cfa72aefe08d944d847a9a8375241fc8dd8
Aragon Etzel <aragon@aragonseye.com>
2022-09-22 15:31:42 +0000

    renaming to ship.py - using this class for all ships.

---
+++ ship.py
@@ -0,0 +1,11 @@
+class Submarine: 
+    def __init__(self,initialPosition):
+        
+        self.pos = initialPosition
+
+
+    def moveShip(self, position, direction, speed):
+        pass
+
+
+
