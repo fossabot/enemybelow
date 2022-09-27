d73b2071c70be095011806e81965783f756e3dec
Aragon Etzel <aragon@aragonseye.com>
2022-09-27 19:05:25 +0000

    added more skeleton methods

--- ship.py
+++ ship.py
@@ -1,11 +1,21 @@
-class Submarine: 
-    def __init__(self,initialPosition):
+class Ship: 
+    def __init__(self,initialPosition,initialDirection):
         
         self.pos = initialPosition
+        self.direction = initialDirection
 
-
-    def moveShip(self, position, direction, speed):
+    def move(self, position, direction, speed):
         pass
+
+  
+    def fire(self, direction, depth):
+        # ship/sub have to be two spaces from each other max
+        # there's a fire-hit-sink or fire-miss-return-fire
+        pass
+
+    
+    
+    
 
 
 
